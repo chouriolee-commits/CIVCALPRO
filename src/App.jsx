3a78e0e0241aa44986d68728b3e27fb5ac034474
chouriolee-commits <chouriolee@gmail.com>
2026-06-11 21:14:06 -0500

    infraestructura organizada

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>CIVCALPRO</h1>
+      <p>Sistema de cálculo para obras civiles</p>
+    </div>
+  )
+}
+
+export default App
