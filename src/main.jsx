2c91b94ba213d3e347a5dccf7b046f82bb24b87a
chouriolee-commits <chouriolee@gmail.com>
2026-07-15 00:06:12 -0500

    refactor-navigations.css

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ import './modulos/styles/components/forms.css'
 import './App.css'
 import './modulos/styles/components/badges.css'
 import './modulos/styles/components/tables.css'
+import './modulos/styles/modules/navigation.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
